a85ae495b911ff38026e2847dcd5080330900f2a
Keerthi Saagar M R <110979773+keerthisaagar@users.noreply.github.com>
2022-08-28 04:30:15 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,12 @@
+class person{
+    constructor(name,age,city,qualification,occupation,income){
+        this.name = name;
+        this.age = age;
+        this.city= city;
+        this.qualification = qualification;
+        this.occupation = occupation;
+        this.income = income;
+    }
+}
+const Sunel = new person ("Sunel",24,"B.Tech","Chennai","hcl","💵💵💵");
+console.log(Sunel, typeof Sunel);
